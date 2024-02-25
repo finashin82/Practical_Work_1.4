e1929ae64bf624ef984232c31cbc299c315a8401
Роман <@ROMAN>
2024-02-25 09:00:18 +0900

    Удалил архивДобавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Запуск программы
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нажмите любую клавишу для продолжения ...");
+
+            Console.ReadKey();
+
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");            
+
+            Console.ReadLine();
+        }
+    }
+}
